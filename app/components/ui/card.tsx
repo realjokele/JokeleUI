4b6869a3ce00798ab954896ec2bfb00ecc598038
Ahrens <FIT0CJA@vw.vwgroup.com>
2025-03-31 13:42:52 +0200

    Modify <Card>

--- app/components/ui/card.tsx
+++ app/components/ui/card.tsx
@@ -1,3 +1,9 @@
+// Changelog
+//
+// 25-03-25:
+// - Remove gap-(--card-spacing) from <Card> and add mt-(--card-spacing) to <CardContent> and <CardFooter> instead,
+//   so a <Form> can include <CardContent> and <CardFooter> and this does not change the layout spacing.
+
 import { twMerge } from "tailwind-merge";
 
 const Card = ({
@@ -8,7 +14,7 @@ const Card = ({
     <div
       data-slot="card"
       className={twMerge(
-        "group/card flex flex-col gap-(--card-spacing) rounded-lg border bg-bg py-(--card-spacing) text-fg shadow-xs [--card-spacing:theme(spacing.6)] has-[table]:overflow-hidden **:data-[slot=table-header]:bg-muted/50 has-[table]:**:data-[slot=card-footer]:border-t **:[table]:overflow-hidden",
+        "group/card flex flex-col  rounded-lg border bg-bg py-(--card-spacing) text-fg shadow-xs [--card-spacing:theme(spacing.6)] has-[table]:overflow-hidden **:data-[slot=table-header]:bg-muted/50 **:[table]:overflow-hidden",
         className
       )}
       {...props}
@@ -100,7 +106,7 @@ const CardContent = ({
     <div
       data-slot="card-content"
       className={twMerge(
-        "px-(--card-spacing) has-[[data-slot=table-header]]:bg-muted/40 has-[table]:p-0 group-has-[table]/card:border-t **:data-[slot=table-cell]:px-(--card-spacing) **:data-[slot=table-column]:px-(--card-spacing) [&:has(table)+[data-slot=card-footer]]:pt-(--card-spacing)",
+        "px-(--card-spacing) has-[[data-slot=table-header]]:bg-muted/40 has-[table]:p-0 group-has-[table]/card:border-t **:data-[slot=table-cell]:px-(--card-spacing) **:data-[slot=table-column]:px-(--card-spacing) mt-(--card-spacing)",
         className
       )}
       {...props}
@@ -116,7 +122,7 @@ const CardFooter = ({
     <div
       data-slot="card-footer"
       className={twMerge(
-        "flex items-center px-(--card-spacing) [.border-t]:pt-6",
+        "flex items-center px-(--card-spacing) [.border-t]:pt-6 mt-(--card-spacing)",
         className
       )}
       {...props}
---
+++ app/routes/card.route.tsx
@@ -0,0 +1,264 @@
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "#/components/ui/card";
+import { Button } from "#/components/ui/button";
+import { TextField } from "#/components/ui/textfield";
+import { Checkbox } from "#/components/ui/checkbox";
+import { Link } from "#/components/ui/link";
+import { Form } from "react-router";
+import { Menu } from "#/components/ui/menu";
+import { Icon } from "#/components/Icon";
+import { Table } from "#/components/Table";
+import { Pagination } from "#/components/ui/pagination";
+
+export default function CardPage() {
+  return (
+    <div className="w-full space-y-8">
+      <div className="text-center text-xl font-bold">Card Page</div>
+
+      <div>
+        <div className="text-lg font-bold w-full text-center">Card</div>
+        <Card className="mx-auto w-full max-w-lg">
+          <CardHeader>
+            <CardTitle>Monthly Report</CardTitle>
+            <CardDescription>Financial summary for June</CardDescription>
+          </CardHeader>
+          <CardContent>
+            The monthly financial report shows a 15% increase in revenue
+            compared to last month.
+          </CardContent>
+          <CardFooter>
+            <Button>View Details</Button>
+          </CardFooter>
+        </Card>
+      </div>
+
+      <div>
+        <div className="text-xl font-bold w-full text-center">
+          Card with Form
+        </div>
+        <Card className="mx-auto w-full max-w-md">
+          <CardHeader>
+            <CardTitle>Login</CardTitle>
+            <CardDescription>
+              Don't loose the level, just keep on going.
+            </CardDescription>
+          </CardHeader>
+          <Form>
+            <CardContent className="space-y-6">
+              <TextField
+                isRequired
+                label="Email"
+                placeholder="Enter your email"
+              />
+              <TextField
+                isRequired
+                label="Password"
+                isRevealable
+                type="password"
+                placeholder="Enter your password"
+              />
+              <div className="flex items-center justify-between">
+                <Checkbox>Remember me</Checkbox>
+                <Link className="text-sm" to="#">
+                  Forgot password?
+                </Link>
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button className="w-full">Login</Button>
+            </CardFooter>
+          </Form>
+        </Card>
+      </div>
+
+      <div>
+        <div className="text-xl font-bold w-full text-center">
+          Card with Action
+        </div>
+        <Card className="mx-auto w-full max-w-md">
+          <CardHeader>
+            <CardTitle>Customers</CardTitle>
+            <CardDescription>
+              Manage and view customer details with available actions aligned to
+              the right.
+            </CardDescription>
+            <CardAction>
+              <Menu>
+                <Button size="small" intent="outline">
+                  Export... <Icon name="chevron-down" />
+                </Button>
+                <Menu.Content placement="bottom end">
+                  <Menu.Item>Export to PDF</Menu.Item>
+                  <Menu.Item>Export to CSV</Menu.Item>
+                </Menu.Content>
+              </Menu>
+            </CardAction>
+          </CardHeader>
+        </Card>
+      </div>
+
+      <div>
+        <div className="text-xl font-bold w-full text-center">
+          Card with Table
+        </div>
+        <Card className="mx-auto w-full max-w-[800px]">
+          <Card.Header>
+            <Card.Title>Users</Card.Title>
+            <Card.Description>
+              Manage users, groups, and roles.
+            </Card.Description>
+          </Card.Header>
+          <Card.Content>
+            <Table aria-label="Users">
+              <Table.Header>
+                <Table.Column>#</Table.Column>
+                <Table.Column>Name</Table.Column>
+                <Table.Column>Gender</Table.Column>
+                <Table.Column>Age</Table.Column>
+                <Table.Column>Occupation</Table.Column>
+                <Table.Column />
+              </Table.Header>
+              <Table.Body>
+                {users.map((item) => (
+                  <Table.Row key={item.id}>
+                    <Table.Cell>{item.id}</Table.Cell>
+                    <Table.Cell>{item.name}</Table.Cell>
+                    <Table.Cell>{item.gender}</Table.Cell>
+                    <Table.Cell>{item.age}</Table.Cell>
+                    <Table.Cell>{item.occupation}</Table.Cell>
+                    <Table.Cell className="flex justify-end">
+                      <Menu>
+                        <Menu.Trigger>
+                          {/* <IconDotsVertical /> */}
+                        </Menu.Trigger>
+                        <Menu.Content placement="left top">
+                          <Menu.Item>{/* <IconEye /> View */}</Menu.Item>
+                          <Menu.Item>{/* <IconHighlight /> Edit */}</Menu.Item>
+                          <Menu.Separator />
+                          <Menu.Item isDanger>
+                            {/* <IconTrash /> Delete */}
+                          </Menu.Item>
+                        </Menu.Content>
+                      </Menu>
+                    </Table.Cell>
+                  </Table.Row>
+                ))}
+              </Table.Body>
+            </Table>
+          </Card.Content>
+          <Card.Footer>
+            <Pagination>
+              <Pagination.List>
+                <Pagination.Item segment="first" />
+                <Pagination.Item segment="previous" />
+                <Pagination.Section className="rounded-lg border lg:hidden">
+                  <Pagination.Item segment="label">1</Pagination.Item>
+                  <Pagination.Item segment="separator" />
+                  <Pagination.Item className="text-muted-fg" segment="label">
+                    {users.length}
+                  </Pagination.Item>
+                </Pagination.Section>
+                <Pagination.Section className="hidden lg:flex" items={pages}>
+                  {(item) => (
+                    <Pagination.Item
+                      id={item.value.toString()}
+                      isCurrent={item.value === 4}
+                      href="#"
+                    >
+                      {item.value}
+                    </Pagination.Item>
+                  )}
+                </Pagination.Section>
+                <Pagination.Item segment="next" />
+                <Pagination.Item segment="last" />
+              </Pagination.List>
+            </Pagination>
+          </Card.Footer>
+        </Card>
+      </div>
+    </div>
+  );
+}
+
+const users = [
+  {
+    id: 1,
+    name: "John Doe",
+    gender: "Male",
+    age: 30,
+    occupation: "Software Engineer",
+  },
+  {
+    id: 2,
+    name: "Jane Smith",
+    gender: "Female",
+    age: 25,
+    occupation: "Marketing Manager",
+  },
+  {
+    id: 3,
+    name: "Bob Johnson",
+    gender: "Male",
+    age: 40,
+    occupation: "Doctor",
+  },
+  {
+    id: 4,
+    name: "Emily Chen",
+    gender: "Female",
+    age: 28,
+    occupation: "Teacher",
+  },
+  {
+    id: 5,
+    name: "Michael Brown",
+    gender: "Male",
+    age: 35,
+    occupation: "Lawyer",
+  },
+  {
+    id: 6,
+    name: "Sarah Lee",
+    gender: "Female",
+    age: 32,
+    occupation: "Designer",
+  },
+  {
+    id: 7,
+    name: "Kevin White",
+    gender: "Male",
+    age: 45,
+    occupation: "CEO",
+  },
+  {
+    id: 8,
+    name: "Lisa Nguyen",
+    gender: "Female",
+    age: 29,
+    occupation: "Engineer",
+  },
+  {
+    id: 9,
+    name: "David Kim",
+    gender: "Male",
+    age: 38,
+    occupation: "Consultant",
+  },
+  {
+    id: 10,
+    name: "Hannah Patel",
+    gender: "Female",
+    age: 26,
+    occupation: "Writer",
+  },
+];
+const pages = Array.from({ length: users.length / 2 }, (_, i) => ({
+  value: i + 1,
+}));
